dd18d98cecc9aa5c5bbbc26350c407bfdc3802bb
Tim te Beek <timtebeek@gmail.com>
2024-10-24 11:34:02 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/org/openrewrite/java/jackson/codehaus/RemoveDoublyAnnotatedCodehausAnnotations.java
+++ src/main/java/org/openrewrite/java/jackson/codehaus/RemoveDoublyAnnotatedCodehausAnnotations.java
@@ -26,7 +26,6 @@ import org.openrewrite.java.service.AnnotationService;
 import org.openrewrite.java.tree.J;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class RemoveDoublyAnnotatedCodehausAnnotations extends Recipe {
