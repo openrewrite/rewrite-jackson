70a2c2be995c49dd6cc2ae8485ddb09fa241a1a3
Tim te Beek <tim@moderne.io>
2024-11-12 11:58:19 +0100

    Migrate additional introspectors

--- src/test/java/org/openrewrite/java/jackson/codehaus/CodehausToFasterXMLTest.java
+++ src/test/java/org/openrewrite/java/jackson/codehaus/CodehausToFasterXMLTest.java
@@ -17,6 +17,8 @@ package org.openrewrite.java.jackson.codehaus;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openrewrite.DocumentExample;
 import org.openrewrite.Issue;
 import org.openrewrite.java.JavaParser;
@@ -333,62 +335,68 @@ class CodehausToFasterXMLTest implements RewriteTest {
     }
 
     @Issue("https://github.com/openrewrite/rewrite-jackson/issues/17")
-    @Issue("https://github.com/openrewrite/rewrite-jackson/issues/17")
-    @Test
-    void replaceWithSetConfigCallGeneric() {
-        rewriteRun(
-          //language=java
-          java(
-            """
-              import org.codehaus.jackson.map.AnnotationIntrospector;
-              import org.codehaus.jackson.map.ObjectMapper;
+    @Nested
+    class AnnotationIntrospector {
 
-              class Test {
-                  void method(ObjectMapper mapper, AnnotationIntrospector introspector) {
-                      mapper.getSerializationConfig().setAnnotationIntrospector(introspector);
+        @Test
+        void replaceWithSetConfigCallGeneric() {
+            rewriteRun(
+              //language=java
+              java(
+                """
+                  import org.codehaus.jackson.map.AnnotationIntrospector;
+                  import org.codehaus.jackson.map.ObjectMapper;
+
+                  class Test {
+                      void method(ObjectMapper mapper, AnnotationIntrospector introspector) {
+                          mapper.getSerializationConfig().setAnnotationIntrospector(introspector);
+                      }
                   }
-              }
-              """,
-            """
-              import com.fasterxml.jackson.databind.AnnotationIntrospector;
-              import com.fasterxml.jackson.databind.ObjectMapper;
+                  """,
+                """
+                  import com.fasterxml.jackson.databind.AnnotationIntrospector;
+                  import com.fasterxml.jackson.databind.ObjectMapper;
 
-              class Test {
-                  void method(ObjectMapper mapper, AnnotationIntrospector introspector) {
-                      mapper.setConfig(mapper.getSerializationConfig().with(introspector));
+                  class Test {
+                      void method(ObjectMapper mapper, AnnotationIntrospector introspector) {
+                          mapper.setConfig(mapper.getSerializationConfig().with(introspector));
+                      }
                   }
-              }
-              """
-          )
-        );
-    }
+                  """
+              )
+            );
+        }
 
-    @Test
-    void replaceWithSetConfigCallJaxB() {
-        rewriteRun(
-          //language=java
-          java(
-            """
-              import org.codehaus.jackson.map.ObjectMapper;
-              import org.codehaus.jackson.xc.JaxbAnnotationIntrospector;
+        @ParameterizedTest
+        @CsvSource(textBlock = """
+          org.codehaus.jackson.xc.JaxbAnnotationIntrospector, com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector
+          org.codehaus.jackson.map.introspect.JacksonAnnotationIntrospector, com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector
+          org.codehaus.jackson.map.introspect.NopAnnotationIntrospector, com.fasterxml.jackson.databind.introspect.NopAnnotationIntrospector
+          """) // org.codehaus.jackson.map.AnnotationIntrospector.Pair, com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair
+        void replaceWithSetConfigCallJaxB(String codehausClass, String fasterXmlClass) {
+            rewriteRun(
+              //language=java
+              java(
+                """
+                  import %s;
 
-              class Test {
-                  void method(ObjectMapper mapper, JaxbAnnotationIntrospector introspector) {
-                      mapper.getSerializationConfig().setAnnotationIntrospector(introspector);
+                  class Test {
+                      void method(org.codehaus.jackson.map.ObjectMapper mapper, %s introspector) {
+                          mapper.getSerializationConfig().setAnnotationIntrospector(introspector);
+                      }
                   }
-              }
-              """,
-            """
-              import com.fasterxml.jackson.databind.ObjectMapper;
-              import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+                  """.formatted(codehausClass, codehausClass.substring(codehausClass.lastIndexOf('.') + 1)),
+                """
+                  import %s;
 
-              class Test {
-                  void method(ObjectMapper mapper, JaxbAnnotationIntrospector introspector) {
-                      mapper.setConfig(mapper.getSerializationConfig().with(introspector));
+                  class Test {
+                      void method(com.fasterxml.jackson.databind.ObjectMapper mapper, %s introspector) {
+                          mapper.setConfig(mapper.getSerializationConfig().with(introspector));
+                      }
                   }
-              }
-              """
-          )
-        );
+                  """.formatted(fasterXmlClass, fasterXmlClass.substring(fasterXmlClass.lastIndexOf('.') + 1))
+              )
+            );
+        }
     }
 }
