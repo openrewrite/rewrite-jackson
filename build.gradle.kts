de17d0c77b64883b9809d4b17e8ea8658b87e70a
Tim te Beek <tim@moderne.io>
2025-07-09 22:33:13 +0200

    Add Codehaus migration recipes

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "org.openrewrite.recipe"
-description = "OpenRewrite recipes for modernizing, upgrading, applying best practices for the fasterxml Jackson serialization/deserialization libraries "
+description = "OpenRewrite recipes for modernizing, upgrading, applying best practices for the fasterxml Jackson serialization/deserialization libraries"
 
 val rewriteVersion = rewriteRecipe.rewriteVersion.get()
 dependencies {
@@ -13,7 +13,6 @@ dependencies {
     implementation("org.openrewrite.recipe:rewrite-java-dependencies:$rewriteVersion")
     implementation("org.assertj:assertj-core:latest.release")
     runtimeOnly("org.openrewrite:rewrite-java-17")
-
 
     annotationProcessor("org.openrewrite:rewrite-templating:$rewriteVersion")
     implementation("org.openrewrite:rewrite-templating:$rewriteVersion")
@@ -26,29 +25,13 @@ dependencies {
     testImplementation("org.openrewrite:rewrite-test")
     testImplementation("org.openrewrite:rewrite-maven")
 
-    runtimeOnly("ch.qos.logback:logback-classic:1.2.+")
+    testRuntimeOnly("org.codehaus.jackson:jackson-core-asl:latest.release")
+    testRuntimeOnly("org.codehaus.jackson:jackson-mapper-asl:latest.release")
+    testRuntimeOnly("org.codehaus.jackson:jackson-xc:latest.release")
 }
 
 recipeDependencies {
     parserClasspath("com.fasterxml.jackson.core:jackson-annotations:2.19.0")
     parserClasspath("com.fasterxml.jackson.core:jackson-core:2.19.0")
     parserClasspath("com.fasterxml.jackson.core:jackson-databind:2.19.0")
-}
-
-signing {
-    // To enable signing have your CI workflow set the "signingKey" and "signingPassword" Gradle project properties
-    isRequired = false
-}
-
-// Use maven-style "SNAPSHOT" versioning for non-release builds
-configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
-    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
-}
-
-configure<PublishingExtension> {
-    publications {
-        named("nebula", MavenPublication::class.java) {
-            suppressPomMetadataWarningsFor("runtimeElements")
-        }
-    }
 }
