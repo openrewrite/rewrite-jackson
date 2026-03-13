64debdd66101f22a581f8ad6f6e6ba7e4cf74508
Tim te Beek <tim@moderne.io>
2026-03-13 19:30:02 +0100

    Add `@JsonCreator` to non-public constructors for Jackson 3 migration (#90)

---
+++ src/main/java/org/openrewrite/java/jackson/AddJsonCreatorToPrivateConstructors.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2026 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.jackson;
+
+import lombok.Getter;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.AnnotationMatcher;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
+import org.openrewrite.java.tree.Statement;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.Collections.singleton;
+
+public class AddJsonCreatorToPrivateConstructors extends Recipe {
+
+    private static final String JACKSON_ANNOTATION_PACKAGE = "com.fasterxml.jackson.annotation";
+    private static final String JACKSON_JSON_CREATOR = JACKSON_ANNOTATION_PACKAGE + ".JsonCreator";
+    private static final AnnotationMatcher JSON_CREATOR_MATCHER = new AnnotationMatcher("@" + JACKSON_JSON_CREATOR, true);
+
+    @Getter
+    final String displayName = "Add `@JsonCreator` to non-public constructors";
+
+    @Getter
+    final String description = "Jackson 3 strictly enforces creator visibility rules. Non-public constructors in " +
+            "Jackson-annotated classes that were auto-detected in Jackson 2 need an explicit `@JsonCreator` " +
+            "annotation to work for deserialization in Jackson 3.";
+
+    @Getter
+    final Set<String> tags = singleton("jackson-3");
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(
+                new UsesType<>(JACKSON_ANNOTATION_PACKAGE + ".*", false),
+                new JavaIsoVisitor<ExecutionContext>() {
+                    @Override
+                    public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+                        getCursor().putMessage("HAS_JACKSON", hasJacksonAnnotation(classDecl));
+                        return super.visitClassDeclaration(classDecl, ctx);
+                    }
+
+                    @Override
+                    public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
+                        J.MethodDeclaration md = super.visitMethodDeclaration(method, ctx);
+
+                        // Must be a constructor
+                        if (!md.isConstructor()) {
+                            return md;
+                        }
+
+                        // Must be non-public
+                        if (md.hasModifier(J.Modifier.Type.Public)) {
+                            return md;
+                        }
+
+                        // Must have parameters
+                        if (md.getParameters().isEmpty() ||
+                                (md.getParameters().size() == 1 && md.getParameters().get(0) instanceof J.Empty)) {
+                            return md;
+                        }
+
+                        // Must not already have @JsonCreator
+                        for (J.Annotation annotation : md.getLeadingAnnotations()) {
+                            if (JSON_CREATOR_MATCHER.matches(annotation)) {
+                                return md;
+                            }
+                        }
+
+                        // Enclosing class must have at least one Jackson annotation
+                        if (!Boolean.TRUE.equals(getCursor().getNearestMessage("HAS_JACKSON"))) {
+                            return md;
+                        }
+
+                        // Add @JsonCreator
+                        maybeAddImport(JACKSON_JSON_CREATOR);
+
+                        return JavaTemplate
+                                .builder("@JsonCreator")
+                                .imports(JACKSON_JSON_CREATOR)
+                                .javaParser(JavaParser.fromJavaVersion()
+                                        .classpathFromResources(ctx, "jackson-annotations-2"))
+                                .build()
+                                .apply(
+                                        getCursor(),
+                                        md.getCoordinates().addAnnotation(Comparator.comparing(J.Annotation::getSimpleName))
+                                );
+                    }
+                }
+        );
+    }
+
+    private static boolean hasJacksonAnnotation(J.ClassDeclaration classDecl) {
+        if (hasJacksonAmong(classDecl.getLeadingAnnotations())) {
+            return true;
+        }
+        for (Statement stmt : classDecl.getBody().getStatements()) {
+            if (stmt instanceof J.VariableDeclarations) {
+                if (hasJacksonAmong(((J.VariableDeclarations) stmt).getLeadingAnnotations())) {
+                    return true;
+                }
+            } else if (stmt instanceof J.MethodDeclaration) {
+                J.MethodDeclaration md = (J.MethodDeclaration) stmt;
+                if (hasJacksonAmong(md.getLeadingAnnotations())) {
+                    return true;
+                }
+                for (Statement param : md.getParameters()) {
+                    if (param instanceof J.VariableDeclarations &&
+                            hasJacksonAmong(((J.VariableDeclarations) param).getLeadingAnnotations())) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean hasJacksonAmong(List<J.Annotation> annotations) {
+        for (J.Annotation annotation : annotations) {
+            JavaType type = annotation.getType();
+            if (type instanceof JavaType.FullyQualified &&
+                    ((JavaType.FullyQualified) type).getPackageName().equals(JACKSON_ANNOTATION_PACKAGE)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/org/openrewrite/java/jackson/AddJsonCreatorToPrivateConstructorsTest.java
@@ -0,0 +1,326 @@
+/*
+ * Copyright 2026 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.jackson;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class AddJsonCreatorToPrivateConstructorsTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new AddJsonCreatorToPrivateConstructors())
+          .parser(JavaParser.fromJavaVersion().classpath("jackson-annotations"));
+    }
+
+    @DocumentExample
+    @Test
+    void privateConstructorWithJsonPropertyParams() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import com.fasterxml.jackson.annotation.JsonProperty;
+
+              class Model {
+                  private final String name;
+                  private final int age;
+
+                  private Model(@JsonProperty("name") String name, @JsonProperty("age") int age) {
+                      this.name = name;
+                      this.age = age;
+                  }
+              }
+              """,
+            """
+              import com.fasterxml.jackson.annotation.JsonCreator;
+              import com.fasterxml.jackson.annotation.JsonProperty;
+
+              class Model {
+                  private final String name;
+                  private final int age;
+
+                  @JsonCreator
+                  private Model(@JsonProperty("name") String name, @JsonProperty("age") int age) {
+                      this.name = name;
+                      this.age = age;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void packagePrivateConstructorWithJsonPropertyParams() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import com.fasterxml.jackson.annotation.JsonProperty;
+
+              class Model {
+                  private final String name;
+
+                  Model(@JsonProperty("name") String name) {
+                      this.name = name;
+                  }
+              }
+              """,
+            """
+              import com.fasterxml.jackson.annotation.JsonCreator;
+              import com.fasterxml.jackson.annotation.JsonProperty;
+
+              class Model {
+                  private final String name;
+
+                  @JsonCreator
+                  Model(@JsonProperty("name") String name) {
+                      this.name = name;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void protectedConstructorWithJsonPropertyParams() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import com.fasterxml.jackson.annotation.JsonProperty;
+
+              class Model {
+                  private final String name;
+
+                  protected Model(@JsonProperty("name") String name) {
+                      this.name = name;
+                  }
+              }
+              """,
+            """
+              import com.fasterxml.jackson.annotation.JsonCreator;
+              import com.fasterxml.jackson.annotation.JsonProperty;
+
+              class Model {
+                  private final String name;
+
+                  @JsonCreator
+                  protected Model(@JsonProperty("name") String name) {
+                      this.name = name;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangePublicConstructor() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import com.fasterxml.jackson.annotation.JsonProperty;
+
+              class Model {
+                  private final String name;
+
+                  public Model(@JsonProperty("name") String name) {
+                      this.name = name;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeAlreadyAnnotatedWithJsonCreator() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import com.fasterxml.jackson.annotation.JsonCreator;
+              import com.fasterxml.jackson.annotation.JsonProperty;
+
+              class Model {
+                  private final String name;
+
+                  @JsonCreator
+                  private Model(@JsonProperty("name") String name) {
+                      this.name = name;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeNoArgConstructor() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import com.fasterxml.jackson.annotation.JsonProperty;
+
+              class Model {
+                  @JsonProperty("name")
+                  private String name;
+
+                  private Model() {
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void addJsonCreatorToConstructorWithoutJsonPropertyOnParamsInJacksonAnnotatedClass() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import com.fasterxml.jackson.annotation.JsonProperty;
+
+              class Model {
+                  @JsonProperty("name")
+                  private String name;
+
+                  private Model(String name) {
+                      this.name = name;
+                  }
+              }
+              """,
+            """
+              import com.fasterxml.jackson.annotation.JsonCreator;
+              import com.fasterxml.jackson.annotation.JsonProperty;
+
+              class Model {
+                  @JsonProperty("name")
+                  private String name;
+
+                  @JsonCreator
+                  private Model(String name) {
+                      this.name = name;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void addJsonCreatorWhenClassHasJsonIgnore() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import com.fasterxml.jackson.annotation.JsonIgnore;
+
+              class Model {
+                  private final String name;
+                  @JsonIgnore
+                  private final String secret;
+
+                  private Model(String name, String secret) {
+                      this.name = name;
+                      this.secret = secret;
+                  }
+              }
+              """,
+            """
+              import com.fasterxml.jackson.annotation.JsonCreator;
+              import com.fasterxml.jackson.annotation.JsonIgnore;
+
+              class Model {
+                  private final String name;
+                  @JsonIgnore
+                  private final String secret;
+
+                  @JsonCreator
+                  private Model(String name, String secret) {
+                      this.name = name;
+                      this.secret = secret;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void addJsonCreatorWhenClassHasClassLevelAnnotation() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import com.fasterxml.jackson.annotation.JsonInclude;
+
+              @JsonInclude(JsonInclude.Include.NON_NULL)
+              class Model {
+                  private final String name;
+
+                  private Model(String name) {
+                      this.name = name;
+                  }
+              }
+              """,
+            """
+              import com.fasterxml.jackson.annotation.JsonCreator;
+              import com.fasterxml.jackson.annotation.JsonInclude;
+
+              @JsonInclude(JsonInclude.Include.NON_NULL)
+              class Model {
+                  private final String name;
+
+                  @JsonCreator
+                  private Model(String name) {
+                      this.name = name;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void doNotChangeClassWithoutJacksonAnnotations() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              class Model {
+                  private final String name;
+
+                  private Model(String name) {
+                      this.name = name;
+                  }
+              }
+              """
+          )
+        );
+    }
+}
